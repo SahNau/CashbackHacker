1459b2928c150566fe55afb9a3d2cee9f04de140
Alexander Naumov <aleksnn00@gmail.com>
2021-05-18 13:41:46 +0500

    finish testing branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,18 +1,36 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
+    //проверка по граничным значениям
     @Test
     void shouldCalculateHowMuchRemainToBonus() {
-        int amount = 700;
+        int amount = 999;
         int actual = service.remain(amount);
-        int expected = 300;
-        assertEquals(expected, actual);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    //проверка по граничным значениям
+    @Test
+    void shouldCalculateHowMuchRemainToBonus2() {
+        int amount = 11;
+        int actual = service.remain(amount);
+        int expected = 989;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldCalculateHowMuchRemainToBonus3() {
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -20,6 +38,6 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
