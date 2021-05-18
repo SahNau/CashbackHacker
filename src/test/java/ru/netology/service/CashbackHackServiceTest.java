c3d76f0b1c65dd59c2895aff9e142f822690300e
Alexander Naumov <aleksnn00@gmail.com>
2021-05-18 14:00:46 +0500

    finish junit4 branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,43 +1,43 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     //проверка по граничным значениям
     @Test
-    void shouldCalculateHowMuchRemainToBonus() {
+    public void shouldCalculateHowMuchRemainToBonus() {
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     //проверка по граничным значениям
     @Test
-    void shouldCalculateHowMuchRemainToBonus2() {
+    public void shouldCalculateHowMuchRemainToBonus2() {
         int amount = 11;
         int actual = service.remain(amount);
         int expected = 989;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateHowMuchRemainToBonus3() {
+    public void shouldCalculateHowMuchRemainToBonus3() {
         int amount = 1001;
         int actual = service.remain(amount);
         int expected = 999;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldMistake() {
+    public void shouldMistake() {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
